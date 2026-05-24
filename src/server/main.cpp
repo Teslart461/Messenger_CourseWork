07ec5d5a2260d2e7dd9dcafe48e612e4d1168b08
Teslart461 <apanteleev1001@gmail.com>
2026-05-24 15:06:12 +0700

    Added database initialization to the main server function

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,6 +1,21 @@
 #include "common/Logger.h"
+#include "server/DataBaseManager.h"
 
 int main() {
     Logger::getInstance().log("Сервер запускается...");
+
+    // Инициализируем базу данных
+    if (!DataBaseManager::getInstance().init()) {
+        Logger::getInstance().log("Не удалось инициализировать базу данных. Завершение работы.", LogLevel::ERROR);
+        return 1;
+    }
+
+    // Здесь будет код для запуска сервера, принятия подключений и обработки сообщений.
+    Logger::getInstance().log("Сервер успешно запущен. Ожидание подключений...");
+
+    // Для демонстрации просто ждем ввода в консоли, чтобы не завершать программу сразу.
+    DataBaseManager::getInstance().close();
+    Logger::getInstance().log("Сервер завершил работу.");
+
     return 0;
 }
